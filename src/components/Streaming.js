e888b78380f7b9587b08b65012399bf07bfb03c3
StolenMoments <unknown9732@naver.com>
2020-02-13 10:43:41 +0900

    jonghwa first commit

---
+++ src/components/Streaming.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Streaming extends React.Component {
+    render(){
+        return (
+            <div>
+                HEllo World!
+            </div>
+        )
+    }
+}
+
+export default Streaming
